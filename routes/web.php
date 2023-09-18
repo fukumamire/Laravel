ea58d513bc7bfc0e8a293744198df68406f181c0
fukumamire <rinka_karyou@hotmail.com>
2023-09-18 19:37:34 +0900

    routes/web.php TestController.phpの書き換え

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-    namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
-    use Illuminate\Http\Request;
-
-    class TestController extends Controller
-    {
-       //
-        public function index()
-        {         return view('index');
-        }
-    }
+Route::get('/', [TestController::class, 'index']);
