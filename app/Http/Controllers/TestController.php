d5ba191f5712d3cc0cf1272217c7e5609111b8d7
fukumamire <rinka_karyou@hotmail.com>
2023-11-18 21:23:59 +0900

    laravel発展　２－２ミドルウェア ビューの作成まで

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,12 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TestController extends Controller {
-
-public function index(Request $request){
-    $item = [
-            'content' => 'パラメータを渡す',
-            'txt' => 'テキスト',
-            'param' => $request->text
+    public function index(){
+        $item = [
+            'content' => '本文'
         ];
         return view('index', $item);
     }
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\MiddlewareController;
 
-// Route::get('/', [TestController::class, 'index']);
+Route::get('/', [TestController::class, 'index']);
 // Route::get('/test', [TestController::class, 'index']);
 
 // ↓任意パラメータ
 // Route::get('/test/{text?}', [TestController::class, 'index']);
 
 // クエリパラメータなのでURLのルートパス　アドレス部分に{パラメータ名}は不要　｛text?}削除↓
-Route::get('/test', [TestController::class, 'index']);
+// Route::get('/test', [TestController::class, 'index']);
 // Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/middleware', [MiddlewareController::class, 'index']);
+Route::post('/middleware', [MiddlewareController::class, 'post'])->middleware('first');
